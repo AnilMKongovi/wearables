5a1ab5029df248f8e160e0fd056ad105d12909af
Anil M Kongovi <invinciblekongovi@gmail.com>
2026-02-21 00:14:27 +0530

    Initial Skeletal for user-service

---
+++ user-service/src/main/java/com/wearables/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 Pranaah Electronics Pvt Ltd. All rights reserved.
+
+ * This source code and all associated files are the proprietary and confidential
+ * property of Pranaah Electronics Pvt Ltd. Unauthorized copying, modification,
+ * distribution, or use of this software, via any medium, is strictly prohibited
+ * without the express written permission of Pranaah Electronics Pvt Ltd.
+*/
+
+
+//package com.pranaah.wearables.auth.application.service;
+
+
